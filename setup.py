946734b8ba7610bb3323fca2e4814bf8266ad328
earthmant <trammell@cloudify.co>
2020-12-08 12:39:00 +0200

    __NODOCS__

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
     install_requires=[
         "cloudify-common>=4.5.5",
         "pyyaml>=4.2b1",
+        "networkx==1.9.1",
         "requests>=2.7.0,<3.0",
         "cloudify-utilities-plugins-sdk>=0.0.31",  # Shared Resource Downloader
     ],
